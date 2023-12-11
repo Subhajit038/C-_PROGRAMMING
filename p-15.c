47b0cd5dd3e9cb8ddc45e3feeab7852f4abbcfaf
SUBHAJIT KHANDA <147081374+Subhajit038@users.noreply.github.com>
2023-12-12 01:06:39 +0530

    Create p-15.c

---
+++ p-15.c
@@ -0,0 +1,13 @@
+//Write a program in c take a input for a user.
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("You entered: %d\n", num);
+
+    return 0;
+}
