688fa63147e609fafa542edf2de51d7e08b19af1
SUBHAJIT KHANDA <147081374+Subhajit038@users.noreply.github.com>
2023-12-12 00:38:42 +0530

    Create p-7.c

---
+++ p-7.c
@@ -0,0 +1,14 @@
+//W.A.P a program in c to display your name n Number of times in for loop.
+#include <stdio.h>
+
+int main() {
+    char name[] = "W.A.P";
+    int number_of_times = 10;
+    int i;
+
+    for (i = 0; i < number_of_times; i++) {
+        printf("%s\n", name);
+    }
+
+    return 0;
+}
