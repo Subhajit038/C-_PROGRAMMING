71dbeb031c580acdfb8c618ef3249201c4b36ecf
SUBHAJIT KHANDA <147081374+Subhajit038@users.noreply.github.com>
2023-12-12 00:50:29 +0530

    Create p-10.c

---
+++ p-10.c
@@ -0,0 +1,19 @@
+//Write a program in c sum of two numbers.
+#include <stdio.h>
+
+int main() 
+    int num1, num2, sum;
+    
+    // Input two numbers
+    printf("Enter first number: ");
+    scanf("
+    printf("Enter second number: ");
+    scanf("
+    
+    // Add the two numbers
+    sum = num1 + num2;
+    
+    // Display the sum
+    printf("Sum of 
+    
+    return 0;
