ce4e51a77159ef572ff22f774914746c8b51b5ff
SUBHAJIT KHANDA <147081374+Subhajit038@users.noreply.github.com>
2023-12-12 00:44:14 +0530

    Create p-8.c

---
+++ p-8.c
@@ -0,0 +1,19 @@
+//Write program in  c to print are of triangle.
+#include <stdio.h>
+
+int main() {
+    float base, height, area;
+
+    // Input base and height of the triangle
+    printf("Enter the base of the triangle: ");
+    scanf("
+    printf("Enter the height of the triangle: ");
+    scanf("
+
+    // Calculate the area of the triangle
+    area = 0.5 * base * height;
+
+    // Print the area of the triangle
+    printf("The area of the triangle is: 
+
+    return 0;
