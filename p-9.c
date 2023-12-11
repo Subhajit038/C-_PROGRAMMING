28d04bb3b730d6895d55da4b7b52445cf4956a10
SUBHAJIT KHANDA <147081374+Subhajit038@users.noreply.github.com>
2023-12-12 00:48:39 +0530

    Create p-9.c

---
+++ p-9.c
@@ -0,0 +1,19 @@
+//Write a program in c to check whether the character entered by the user is a vowel or not. 
+#include <stdio.h>
+
+int main() 
+    char ch;
+
+    // Input the character
+    printf("Enter a character: ");
+    scanf(" 
+
+    // Check if the character is a vowel
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') 
+        printf("
+     else 
+        printf("
+    
+
+    return 0;
