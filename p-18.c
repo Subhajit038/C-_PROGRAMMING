afc10274b537dad9234f06cee9c944f8fe11875e
SUBHAJIT KHANDA <147081374+Subhajit038@users.noreply.github.com>
2023-12-12 01:24:14 +0530

    Create p-18.c

---
+++ p-18.c
@@ -0,0 +1,17 @@
+//Write a program in c to check whether a number is odd or even.
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("%d is an even number.\n", number);
+    } else {
+        printf("%d is an odd number.\n", number);
+    }
+
+    return 0;
+}
